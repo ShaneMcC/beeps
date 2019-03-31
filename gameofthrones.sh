f7f676439452ce6c8417749df8070d22f63a4ddf
Daniel Kamil Kozar <dkk089@gmail.com>
2019-04-01 00:08:42 +0200

    Add the theme from "Game of Thrones"

---
+++ gameofthrones.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+# converted from C code available at
+# https://www.instructables.com/id/Game-Of-Thrones-Theme-on-Arduino/
+
+NOTE_F3=175
+NOTE_G3=196
+NOTE_GS3=208
+NOTE_AS3=233
+NOTE_C4=262
+NOTE_D4=294
+NOTE_DS4=311
+NOTE_E4=330
+NOTE_F4=349
+NOTE_G4=392
+  
+beep \
+  -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_E4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_E4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_E4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_E4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 500 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 1000 \
+  -n -f $NOTE_F4 -l 1000 \
+  -n -f $NOTE_AS3 -l 1000 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_D4 -l 250 \
+  -n -f $NOTE_F4 -l 1000 \
+  -n -f $NOTE_AS3 -l 1000 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_D4 -l 250 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_GS3 -l 250 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_F3 -l 500 \
+  -n -f $NOTE_GS3 -l 250 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_F3 -l 500 \
+  -n -f $NOTE_GS3 -l 250 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 500 \
+  -n -f $NOTE_F3 -l 500 \
+  -n -f $NOTE_G4 -l 1000 \
+  -n -f $NOTE_C4 -l 1000 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_G4 -l 1000 \
+  -n -f $NOTE_C4 -l 1000 \
+  -n -f $NOTE_DS4 -l 250 \
+  -n -f $NOTE_F4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 500 \
+  -n -f $NOTE_G3 -l 500 \
+  -n -f $NOTE_AS3 -l 250 \
+  -n -f $NOTE_C4 -l 250 \
+  -n -f $NOTE_D4 -l 500
