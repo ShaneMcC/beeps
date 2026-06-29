a2a9bacf309f76ce4ce84cce2d1f3e026e5600d9
Lorenzo Moscati <lo.mosca.ti@gmail.com>
2026-06-30 01:15:41 +0200

    Add odetojoy.sh

---
+++ odetojoy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+beep \
+  `# A  bar 1:  E E F G` \
+  -f 330 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 349 -l 460 -D 40 -n -f 392 -l 460 -D 40 -n \
+  `#    bar 2:  G F E D` \
+  -f 392 -l 460 -D 40 -n -f 349 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n \
+  `#    bar 3:  C C D E` \
+  -f 262 -l 460 -D 40 -n -f 262 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n \
+  `#    bar 4:  E. D D  (dotted-q, 8th, half)` \
+  -f 330 -l 710 -D 40 -n -f 294 -l 210 -D 40 -n -f 294 -l 960 -D 40 -n \
+  `# A' bar 5:  E E F G` \
+  -f 330 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 349 -l 460 -D 40 -n -f 392 -l 460 -D 40 -n \
+  `#    bar 6:  G F E D` \
+  -f 392 -l 460 -D 40 -n -f 349 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n \
+  `#    bar 7:  C C D E` \
+  -f 262 -l 460 -D 40 -n -f 262 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n \
+  `#    bar 8:  D. C C  (dotted-q, 8th, half)` \
+  -f 294 -l 710 -D 40 -n -f 262 -l 210 -D 40 -n -f 262 -l 960 -D 40 -n \
+  `# B  bar 9:  D D E C` \
+  -f 294 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 262 -l 460 -D 40 -n \
+  `#    bar 10: D E F E C  (F-E = 8ths)` \
+  -f 294 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 349 -l 210 -D 40 -n -f 330 -l 210 -D 40 -n -f 262 -l 460 -D 40 -n \
+  `#    bar 11: D E F E D  (F-E = 8ths)` \
+  -f 294 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 349 -l 210 -D 40 -n -f 330 -l 210 -D 40 -n -f 294 -l 460 -D 40 -n \
+  `#    bar 12: C D G  (low G = traditional descent)` \
+  -f 262 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n -f 196 -l 960 -D 40 -n \
+  `# A' bar 13: E E F G` \
+  -f 330 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 349 -l 460 -D 40 -n -f 392 -l 460 -D 40 -n \
+  `#    bar 14: G F E D` \
+  -f 392 -l 460 -D 40 -n -f 349 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n \
+  `#    bar 15: C C D E` \
+  -f 262 -l 460 -D 40 -n -f 262 -l 460 -D 40 -n -f 294 -l 460 -D 40 -n -f 330 -l 460 -D 40 -n \
+  `#    bar 16: D. C C  (dotted-q, 8th, half)` \
+  -f 294 -l 710 -D 40 -n -f 262 -l 210 -D 40 -n -f 262 -l 960 -D 40
